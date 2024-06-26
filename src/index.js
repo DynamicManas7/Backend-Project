0c83ba748c13a27d950aa347ecd85199d03288a5
Manas Singh <mk9782647@gmail.com>
2024-06-26 21:48:00 +0530

    connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-// index.js
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config();
 connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // import express from "express"
 // const app=express()
